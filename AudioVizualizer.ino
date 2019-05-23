22fc63885d5f225d3430420ceff3330a65f46a0c
bjpad <50940556+bjpad@users.noreply.github.com>
2019-05-23 01:21:51 -0700

    Update AudioVizualizer.ino

--- AudioVizualizer.ino
+++ AudioVizualizer.ino
@@ -1,6 +1,24 @@
 
 #include <FastLED.h>
 
+#define NumLEDS 300
+#define DataPin 6
+
 void setup() {
+  Serial.begin(3600);
+  
+  //Telling the library there is a strand of LEDs on pin 6
+  //Creating array "leds"
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NumLEDS);
+  
+}
+
+void TestLEDs() {
+  for (int i = 1; i < NumLEDS; i++) {
+    leds[i] = CRGB(100, 100, 100);
+    leds[i - 1] = CRGB(0, 0, 0);
+    FastLED.show();
+    delay(5);
+  }
   
 }

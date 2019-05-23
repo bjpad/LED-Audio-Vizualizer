a9c4b92a263679485af0cddc3126c2d1eaa178f5
bjpad <50940556+bjpad@users.noreply.github.com>
2019-05-23 02:15:15 -0700

    Update AudioVizualizer.ino

--- AudioVizualizer.ino
+++ AudioVizualizer.ino
@@ -3,6 +3,7 @@
 
 #define NumLEDS 300
 #define DataPin 6
+int LoopColorChange = 255/NumLEDS;
 
 void setup() {
   Serial.begin(3600);
@@ -10,15 +11,22 @@ void setup() {
   //Telling the library there is a strand of LEDs on pin 6
   //Creating array "leds"
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NumLEDS);
+  TestLEDS();
+  
   
 }
-
-void TestLEDs() {
-  for (int i = 1; i < NumLEDS; i++) {
-    leds[i] = CRGB(100, 100, 100);
+//////////////////////////////////////////////////////////////////
+void TestLEDS() {
+  for (int i = 0; i < NumLEDS; i++) {
+    leds[i] = CRGB(100, 100, i*LoopColorChange);
     leds[i - 1] = CRGB(0, 0, 0);
     FastLED.show();
     delay(5);
   }
-  
+  for (int i = NumLEDS; i > 0;i--) {
+    leds[i] = CRGB(100, 100, i*LoopColorChange);
+    leds[i+1] = CRGB(0, 0, 0);
+    FastLED.show();
+    delay(5)
+  } 
 }
